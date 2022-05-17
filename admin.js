b0b25baa369ccd498b7bd49746322313cf82ce83
mrmojololo <73600819+mrmojololo@users.noreply.github.com>
2022-05-17 19:33:33 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,9 +2,5 @@ setInterval(function(){alert("Loading Hacks..."); }, 500);
 for (var i = 5; i > 3; i = i + 1)
   { console.log(i); }
 [...Array(2**32-1)].map(_=>Math.ceil(Math.random()*111))
-a='5';while(true)a=a+a+a+a+a+a+a;
-txt = "a";
-while(1){
-    txt = txt += "a";
-}
+
 
